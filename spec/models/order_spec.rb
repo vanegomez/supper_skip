f6663f990b76d6fa364ca4937933c7ab8de65536
vanegomez <vane.gomez@me.com>
2015-07-08 06:41:09 -0600

    add tests for order model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -58,9 +58,11 @@ RSpec.describe Order, :type => :model do
     order_1 = Order.create(order_status: Order::Status::PAID, user_id: 1, order_total: 23, order_type: "pick-up")
     order_2 = Order.create(order_status: Order::Status::ORDERED, user_id: 2, order_total: 12, order_type: "pick-up")
     order_3 = Order.create(order_status: Order::Status::CANCELLED, user_id: 3, order_total: 15, order_type: "pick-up")
+    order_4 = Order.create(order_status: Order::Status::COMPLETED, user_id: 4, order_total: 10, order_type: "pick-up")
 
     order_1.paid?
     order_2.ordered?
     order_3.cancelled?
+    order_4.complete?
   end
 end
