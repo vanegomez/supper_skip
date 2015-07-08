4d01bb195472b70b3e0277c876240b541b5217ed
Jack Yeh <jack.yeh@gmail.com>
2015-07-07 21:19:34 -0600

    improved test coverage by writing test for the create action in the sessions controller

--- spec/features/user_view_spec.rb
+++ spec/features/user_view_spec.rb
@@ -2,9 +2,33 @@ require 'rails_helper'
 require 'capybara/rails'
 require 'capybara/rspec'
 
-describe 'a user viewing the items page', type: :feature do
-  it "can view categories" do
-    #
+describe 'a user signs in and views their user page', type: :feature do
+  let(:user) do
+    User.create!(:full_name   => "john doe",
+                 :email        => "john_doe@example.com",
+                 :display_name => "john_doe_123",
+                 :password     => 'password')
+  end
+
+  context "non-admin user functionality" do
+    it 'can login with the correct information' do
+      page.visit signin_path
+      page.fill_in('session[email]',    with: user.email)
+      page.fill_in('session[password]', with: 'password')
+      page.click_button('Sign in')
+      expect(page.current_path).to eq(user_path(user))
+    end
+  end
+
+  context "invalid user functionality" do
+    it 'cannot login with the incorrect information' do
+      page.visit signin_path
+      page.fill_in('session[email]',    with: '')
+      page.fill_in('session[password]', with: '')
+      page.click_button('Sign in')
+      expect(page.current_path).to eq(sessions_path)
+    end
+
   end
 
   it "can view one category with multiple items in it" do
