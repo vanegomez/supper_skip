876c5c38fefdfdbd71744123957c64fa2586bfd7
vanegomez <vane.gomez@me.com>
2015-07-13 14:04:26 -0600

    commit before switching branches

--- spec/features/registered_owner_can_view_restaurant_url_spec.rb
+++ spec/features/registered_owner_can_view_restaurant_url_spec.rb
@@ -3,15 +3,15 @@ require 'capybara/rails'
 require 'capybara/rspec'
 
 describe 'a registered owner', type: :feature do
-  before(:each) do
-    # @restaurant = Restaurant.create!(name: "Very Sweet", slug: "very-sweet" )
-
-    category = Category.create!(name: "Fruity")
-
-    item = category.items.create(title: "Berry", description: "Three berry blend, of in-season, local berries", inventory: 4, price_pie: 29.99, category_ids: [1])
-
-
-  end
+  # before(:each) do
+  #   # @restaurant = Restaurant.create!(name: "Very Sweet", slug: "very-sweet" )
+  #
+  #   category = Category.create!(name: "Fruity")
+  #
+  #   item = category.items.create(title: "Berry", description: "Three berry blend, of in-season, local berries", inventory: 4, price_pie: 29.99, category_ids: [1])
+  #
+  #
+  # end
 
   it "can view registered restaurant" do
     role = Role.create(name: "customer")
