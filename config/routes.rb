463e7031d7c1082aea4b19fef38c08916d4b2b59
margie <ly.margarett@gmail.com>
2015-07-13 16:58:20 -0600

    restructuring routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,27 +6,30 @@ Rails.application.routes.draw do
   get '/:slug.html' => 'restaurants#show', as: :restaurant
 
   namespace :restaurants, as: :restaurant, path: '/:slug.html' do
-    resources :users
-    resources :orders#, [:new, :create, :show]
-    resources :categories#, only: [:show, :index]
-    resources :items#, only: [:show, :index]
+    resources :categories, only: [:show, :index]
+    resources :items, only: [:show, :index]
   end
 
   # resources :items, only: [:show, :index]
   # resources :categories, only: [:show, :index]
-  # resources :orders, only: [:new, :create, :show]
+  resources :orders, only: [:new, :create, :show]
 
+  resources :users
+
+# 3 admin routes below belong to super admin
   patch 'admin/order/:id/cancel' => 'admin/orders#cancel',   as: :cancel_order
   patch 'admin/pay/:id/pay'      => 'admin/orders#pay',      as: :pay_order
   patch 'admin/pay/:id/complete' => 'admin/orders#complete', as: :complete_order
 
-  resources :users
+  # need super admin controller and restaurant admin controller for different dashboards
+
   resources :sessions, only: [:new, :create, :destroy]
 
   match '/signup',  to: 'users#new',        via: 'get'
   match '/signin',  to: 'sessions#new',     via: 'get'
   match '/signout', to: 'sessions#destroy', via: 'delete'
 
+# carts will be nested under session cookie
   get     '/cart'             => 'carts#show',              as: :cart
   get     '/add_to_cart'      => 'carts#add_to_cart_view',  as: :add_to_cart
 
@@ -36,7 +39,9 @@ Rails.application.routes.draw do
   post    '/cart/items'       => 'cart_items#create',       as: :cart_items
   delete  '/cart/items'       => 'cart_items#destroy',      as: :cart_items_destroy
 
+# restaurant admin
   put     '/admin/retire/item'      => 'admin/retire_item#update'
+
 
   get     '/order_items/new'  => 'order_items#new',         as: :new_order_item
   post    '/order_items'      => 'order_items#create',      as: :order_items
